fe5cd3d7ac4cc795f29a82ade5a149f062c44240
KanjikaSingh <117390540+KanjikaSingh@users.noreply.github.com>
2022-12-09 15:47:57 +0530

    Add files via upload

---
+++ leetcode.c
@@ -0,0 +1,32 @@
+int* canSeePersonsCount(int* heights, int heightsSize, int* returnSize)
+{   int i,j,large=0;
+    int sol[heightsSize];
+    for(int i = 0; i<heightsSize;i++)
+    {
+        large = 0;
+        int maxheight = 0;
+        for(int j =i+1; j<heightsSize;j++)
+    {
+        if(heights[i] < heights[j])
+        {
+            large++;
+            break;
+        }
+        else if((heights[j] > maxheight) && (heights[i] > maxheight))
+        {
+            maxheight = heights[j];
+            large++;
+        }
+    }
+    sol[i] = large;
+}
+
+ for(i=0;i<heightsSize;i++)
+ {
+ printf("%d ",sol[i]);   
+ }
+
+//int *arr = (int) malloc(sizeof(int)*heightsSize);
+//return arr;
+return sol;
+}
